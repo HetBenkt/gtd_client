8932e8a27c20142ecf3113d445eaad8342abf945
HetBenkt <HetBenkt@gmail.com>
2017-12-23 13:53:08 +0100

    Initial commit

---
+++ src/main/java/nl/bos/gtd_client/GtdClientApplication.java
@@ -0,0 +1,12 @@
+package nl.bos.gtd_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtdClientApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/bos/gtd_client/GtdClientApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.bos.gtd_client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GtdClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
